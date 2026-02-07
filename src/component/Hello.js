4ba67c2ebdfc0aff1d561b84b4049f942da7eaf5
최인호 <choiinho97@naver.com>
2026-02-07 21:52:43 +0900

    Feat : 각 컴포넌트별 style 적용하는 방법

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,11 +1,17 @@
 import World from '../component/World'
-
+import styles from "./Hello.module.css"
 const Hello = () => {
   return (
     <div>
-      <h1>Hello</h1>
-      <World />
-      <World />
+      <h1 style={
+        {
+          color: '#f00',
+          border: '2px solid #000',
+          marginBottom: '30px',
+          opacity: 0.7,
+        }
+      }>Hello</h1>
+      <div className={styles.box}>Hello</div>
     </div>
   );
 };
