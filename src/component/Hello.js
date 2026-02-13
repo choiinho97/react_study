e4a70b4cba2b88e6ab66e0a9f61d67bd883824cc
Choi Inho <choiinho97@naver.com>
2026-02-13 14:11:14 +0900

    스타일 입히는 방법

--- src/component/Hello.js
+++ src/component/Hello.js
@@ -1,11 +1,16 @@
 import World from '../component/World'
+import styles from "./Hello.module.css"
 
 const Hello = () => {
   return (
     <div>
-      <h1>Hello</h1>
-      <World />
-      <World />
+      <h1 style={{
+        color: "#ff0",
+        borderRight : "12px solid #000",
+        marginBottom: "50px",
+        opacity: 1,
+      }}>Hello</h1>
+      <div className={styles.box}>Hello</div>
     </div>
   );
 };
