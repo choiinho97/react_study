27d6655e68f7edf61d6a8b0fabd405115a0f7d3e
Choi Inho <choiinho97@naver.com>
2026-02-15 00:15:32 +0900

    Router 구현

--- src/App.js
+++ src/App.js
@@ -1,14 +1,21 @@
 import DayList from "./component/DayList";
 import Header from "./component/Header";
 import Day from "./component/Day";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import EmptyPage from "./component/EmptyPage";
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <DayList />
-      <Day />
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <Header />
+        <Routes>
+          <Route path="/" element={<DayList />} />
+          <Route path="/day/:day" element={<Day />} />
+          <Route path="*" element={<EmptyPage />}></Route>
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 
