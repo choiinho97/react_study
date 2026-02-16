78b7d56435f8e2e5f1a95795fd1995fffa424737
Choi Inho <choiinho97@naver.com>
2026-02-16 23:43:05 +0900

    REST API

--- src/component/Day.js
+++ src/component/Day.js
@@ -1,5 +1,6 @@
 import dummy from "../db/data.json";
 import { useParams } from "react-router-dom";
+import Word from "./Word";
 
 function Day() {
   const a = useParams();
@@ -12,10 +13,7 @@ function Day() {
       <table>
         <tbody>
           {wordList.map((word) => (
-            <tr key={word.id}>
-              <td>{word.eng}</td>
-              <td>{word.kor}</td>
-            </tr>
+            <Word word={word} key={word.id} />
           ))}
         </tbody>
       </table>
