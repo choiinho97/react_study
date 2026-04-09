9885811ea61f126ca3d8ed7fe22c85574b9d578c
최인호 <choiinho97@naver.com>
2026-04-09 23:30:37 +0900

    Feat : 숫자 변경에 따른 변화확인

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <h3>props : properties</h3>
-      <Hello age={20}/>
-      <Hello age={40}/>
+      <Hello age={30}/>
+      <Hello age={50}/>
       <Hello age={70}/>
     </div>
   );
